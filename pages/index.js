949e4dfbd8bb137cc81e2cebbc1a6019394a1f49
Patrick Roth <patrockdev@icloud.com>
2021-05-21 15:59:21 +0200

    redux ssr

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,33 @@
+import React from 'react';
 import styles from './index.module.css';
 import Card from './Card';
+import { initStore, initialCards, addItem } from '../store';
 
-export default () => {
+
+class Index extends React.Component {
+    static async getInitialProps ({ store }) {
+        return store.dispatch(initialCards());  
+    }
+    
+    render () {
     return (
-        <div className={styles.app}>
-            <header className={styles.header}>
-                <img src="/logo.png"
-                    className={styles.logo} alt="logo"
-                />
-           </header>
-           <div className={styles.grid}>
-               <Card />
-               <Card />
-               <Card />
-           </div>
-        </div>
-    )
+            <div className={styles.app}>
+                <header className={styles.header}>
+                    <img src="/logo.png"
+                        className={styles.logo} alt="logo"
+                    />
+                </header>
+                <div className={styles.grid}>
+                    {
+                        this.props.cards.map((card) => (
+                            <Card key={card.id} />
+                        ))
+                    }
+                </div>
+                {/* <button onClick={() => dispatch(addItem())}></button> */}
+            </div>
+        )
+    }  
 };
+
+export default initStore.withRedux(Index);
