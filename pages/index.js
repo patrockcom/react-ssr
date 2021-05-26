2568322fd3855fa8d297e8cc7864751455b43136
Patrick Roth <patrockdev@icloud.com>
2021-05-26 15:45:02 +0200

    rendering with node

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,32 @@
 import React from 'react';
+import Link from 'next/link';
 import styles from './index.module.css';
 import Card from './Card';
-import { initStore, initialCards, addItem } from '../store';
+import data from './API/data.json';
 
-
-class Index extends React.Component {
-    static async getInitialProps ({ store }) {
-        return store.dispatch(initialCards());  
+export default class Index extends React.Component {
+    static async getInitialProps () {
+        return { cards: data }
     }
-    
+
     render () {
-    return (
+        return (
             <div className={styles.app}>
                 <header className={styles.header}>
+                <Link href="/page3">
                     <img src="/logo.png"
                         className={styles.logo} alt="logo"
                     />
+                </Link>    
                 </header>
                 <div className={styles.grid}>
-                    {
-                        this.props.cards.map((card) => (
-                            <Card key={card.id} />
-                        ))
-                    }
+                   {
+                       this.props.cards.map((card) => (
+                           <Card key={card.id} />
+                       ))
+                   }
                 </div>
-                {/* <button onClick={() => dispatch(addItem())}></button> */}
             </div>
         )
-    }  
+    }
 };
-
-export default initStore.withRedux(Index);
